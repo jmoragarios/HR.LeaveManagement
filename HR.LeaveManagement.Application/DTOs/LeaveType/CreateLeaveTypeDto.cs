4391438bb6a3925b7147284907c8e386921da64b
jmoragarios <jmoragarios@gmail.com>
2022-02-03 23:54:24 -0600

    Add project files.

---
+++ HR.LeaveManagement.Application/DTOs/LeaveType/CreateLeaveTypeDto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace HR.LeaveManagement.Application.DTOs.LeaveType
+{
+    public class CreateLeaveTypeDto
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+    }
+}
---
+++ HR.LeaveManagement.Application/Persistence/Contracts/ILeaveRequestRepository.cs
@@ -0,0 +1,13 @@
+﻿using HR.LeaveManagement.Domain;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Persistence.Contracts
+{
+    public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
+    {
+        Task<LeaveRequest> GetLeaveRequestWithDetails(int id);
+        Task<List<LeaveRequest>> GetLeaveRequestsWithDetails();
+        Task ChangeApprovalStatus(LeaveRequest leaveRequest, bool? approved);
+    }
+}
